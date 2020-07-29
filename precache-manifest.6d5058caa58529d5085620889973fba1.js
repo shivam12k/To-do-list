c3b4ad8a369fc202866b9c1ea73aa3aa290e5647
shivam <63141401+shivam12k@users.noreply.github.com>
2020-07-29 17:12:57 +0530

    Updates

---
+++ precache-manifest.6d5058caa58529d5085620889973fba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5986b7fbfcb5574b2a9b8afcf5e8e248",
+    "url": "/To-do-list/index.html"
+  },
+  {
+    "revision": "dd48f88956f2ee3350f5",
+    "url": "/To-do-list/static/css/main.a49e064f.chunk.css"
+  },
+  {
+    "revision": "2daed07f1dd9a7dd9058",
+    "url": "/To-do-list/static/js/2.e87f254a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-list/static/js/2.e87f254a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd48f88956f2ee3350f5",
+    "url": "/To-do-list/static/js/main.6e21b28c.chunk.js"
+  },
+  {
+    "revision": "0cef3ae899cc3e3d1393",
+    "url": "/To-do-list/static/js/runtime-main.40a77e8d.js"
+  }
+]);
